a851da094725ca509753b9c105e7af34dbc26fde
warrior-0 <bodla0330@gmail.com>
2025-06-09 16:16:22 +0900

    Update urban.js

--- urban.js
+++ urban.js
@@ -322,11 +322,6 @@ export const urbanData = [
   }
 ];
 
-urbanData.forEach(async (story) => {
-  await setDoc(doc(db, "urbanStories", String(story.id)), story);
-  console.log(`등록됨: ${story.title}`);
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   if (document.getElementById('urbanList')) {
     let sortType = 'latest';
